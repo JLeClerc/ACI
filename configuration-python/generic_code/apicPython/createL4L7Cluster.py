f4c7832c61a71ffe62f0fe5e7001804d8308c00f
Joseph LeClerc <jolecler@cisco.com>
2015-09-23 15:17:24 -0700

    Add optional args to L4L7Cluster.

--- configuration-python/generic_code/apicPython/createL4L7Cluster.py
+++ configuration-python/generic_code/apicPython/createL4L7Cluster.py
@@ -6,13 +6,20 @@ def input_key_args():
     return input_raw_input('Device Cluster Name', required=True)
 
 def input_optional_args():
-    args = {}
-    # TODO
+    args = {
+        'context_aware': input_options('Context Aware', default='single-Context', options=['single-Context', 'multi-Context']),
+        'device_type': input_options('Device Type', default='PHYSICAL', options=['PHYSICAL', 'VIRTUAL']),
+        'function_type': input_options('Function Type', default='GoTo', options=['GoTo','GoThrough']),
+    }
     return args
 
 def create_l4l7_cluster(fv_tenant, name, **args):
     """Create L4L7 Cluster"""
-    return LDevVip(fv_tenant, name)
+    args = args['optional_args'] if 'optional_args' in args.keys() else args
+    return LDevVip(fv_tenant, name, 
+        contextAware=args['context_aware'],
+        devtype=args['device_type'],
+        funcType=args['function_type'])
 
 class CreateL4L7Cluster(CreateMo):
     def __init__(self):
@@ -24,6 +31,10 @@ class CreateL4L7Cluster(CreateMo):
     def set_cli_mode(self):
         super(CreateL4L7Cluster, self).set_cli_mode()
         self.parser_cli.add_argument('name', help='Cluster Name')
+        self.parser_cli.add_argument('-c', '--context_aware', default='single-Context', choices=['single-Context', 'multi-Context'], 
+            help='The context-awareness of the Device Cluster. Single means that the device cluster cannot be shared across multiple tenants of a given type that are hosted on the provider network. Multiple means that the device cluster can be shared across multiple tenants of a given type that you are hosting on this provider network. ')
+        self.parser_cli.add_argument('-d', '--device_type', default='PHYSICAL', choices=['PHYSICAL', 'VIRTUAL'], help='Specifies whether the device cluster has PHYSICAL appliances or VIRTUAL appliances.')
+        self.parser_cli.add_argument('-f', '--function_type', default='GoThrough', choices=['GoTo','GoThrough'], help='A GoTo device has a specific destination, depending on the package. A GoThrough device is a transparent device.')
 
     def read_key_args(self):
         self.tenant = self.args.pop('tenant')
@@ -39,9 +50,7 @@ class CreateL4L7Cluster(CreateMo):
         super(CreateL4L7Cluster, self).delete_mo()
 
     def main_function(self):
-        # Query a tenant
         fv_tenant = self.check_if_tenant_exist()
-
         vns_ldevvip = create_l4l7_cluster(fv_tenant, self.name, optional_args=self.optional_args)
 
 if __name__ == '__main__':
